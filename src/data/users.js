68be7b0f2e2d8b5964f2d2091713d2fcb771889b
Kuba <kubabednarczyk2003@gmail.com>
2022-06-22 22:44:43 +0200

    make UsersList and UsersLists components

---
+++ src/data/users.js
@@ -0,0 +1,51 @@
+export const users = [
+  {
+    name: 'Adam Romański',
+    attendance: '39%',
+    average: '2.4',
+  },
+  {
+    name: 'Steve Jobs',
+    attendance: '98%',
+    average: '5.1',
+  },
+  {
+    name: 'Krzysztof Batko',
+    attendance: '80%',
+    average: '3.7',
+  },
+  {
+    name: 'Szymon Knut',
+    attendance: '76%',
+    average: '4.8',
+  },
+  {
+    name: 'Olga Khan',
+    attendance: '50%',
+    average: '3.1',
+  },
+  {
+    name: 'Krystian Fortepianiak',
+    attendance: '23%',
+    average: '1.4',
+  },
+  {
+    name: 'Norbert Niewiadomski',
+    attendance: '90%',
+    average: '5.0',
+  },
+  {
+    name: 'Adam Rogult',
+    attendance: '45%',
+    average: '2.4',
+  },
+  {
+    name: 'Anna Jędrzejczyk',
+    attendance: '57%',
+    average: '2.4',
+  },
+  {
+    name: 'Paweł Ciemny',
+    average: '2.4',
+  },
+];
