71806ff1137e3dd33a6d2bdce066ca0c4fc36918
Kuba <kubabednarczyk2003@gmail.com>
2022-06-28 18:34:32 +0200

    event handling in react

--- src/components/molecules/UsersListItem/UsersListItem.js
+++ src/components/molecules/UsersListItem/UsersListItem.js
@@ -3,14 +3,16 @@ import PropTypes from 'prop-types';
 import { StyledAverage, StyledInfo, Wrapper } from './UsersListItem.styles';
 import Button from 'components/atoms/Button/Button';
 
-const UsersListItem = ({ userData: { average, name, attendance = '0%' } }) => (
+const showIndex = (index) => alert(`This is student #${index + 1}`);
+
+const UsersListItem = ({ index, userData: { average, name, attendance = '0%' } }) => (
   <Wrapper>
     <StyledAverage value={average}>{average}</StyledAverage>
     <StyledInfo>
       <p>{name}</p>
       <p>attendance: {attendance}</p>
     </StyledInfo>
-    <Button></Button>
+    <Button onMouseEnter={() => showIndex(index)} />
   </Wrapper>
 );
 
