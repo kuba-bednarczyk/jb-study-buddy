335435e354f71eb2eaa83fb1b8aa70c81c678072
Kuba <kubabednarczyk2003@gmail.com>
2022-06-30 16:13:13 +0200

    end playing with useState and useEffect hooks

--- src/components/molecules/UsersListItem/UsersListItem.js
+++ src/components/molecules/UsersListItem/UsersListItem.js
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types';
 import { StyledAverage, StyledInfo, Wrapper } from './UsersListItem.styles';
 import Button from 'components/atoms/Button/Button';
 
-const UsersListItem = ({ userData: { average, name, attendance = '0%' } }) => (
+const UsersListItem = ({ deleteUser, userData: { average, name, attendance = '0%' } }) => (
   <Wrapper>
     <StyledAverage value={average}>{average}</StyledAverage>
     <StyledInfo>
       <p>{name}</p>
       <p>attendance: {attendance}</p>
     </StyledInfo>
-    <Button></Button>
+    <Button onClick={() => deleteUser(name)}></Button>
   </Wrapper>
 );
 
