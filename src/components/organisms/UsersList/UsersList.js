ed0b02ab4e6893fd03f14137117ac04c0a8ca86c
Kuba <kubabednarczyk2003@gmail.com>
2022-06-30 16:28:15 +0200

    make StyledTitle component

--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -1,48 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { users as usersData } from 'data/users';
 import UsersListItem from 'components/molecules/UsersListItem/UsersListItem';
-import { Wrapper, StyledList } from './UsersList.styles';
-
-const mockAPI = (success) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (usersData) {
-        resolve([...usersData]);
-      } else {
-        reject({ message: 'Error' });
-      }
-    }, 2000);
-  });
-};
+import { Wrapper, StyledList, StyledTitle } from './UsersList.styles';
 
 const UsersList = () => {
-  const [users, setUsers] = useState([]);
-  const [isLoading, setLoadingState] = useState([]);
-
-  useEffect(() => {
-    setLoadingState(true);
-    mockAPI()
-      .then((data) => {
-        setLoadingState(false);
-        setUsers(data);
-      })
-      .catch((err) => console.log(err));
-  }, []);
+  const [users, setUsers] = useState(usersData);
 
   const deleteUser = (name) => {
     const filteredUsers = users.filter((user) => user.name !== name);
     setUsers(filteredUsers);
   };
 
-  const [divAppearance, setDivAppearance] = useState(true);
-
-  const toggleDiv = () => setDivAppearance(!divAppearance);
-
   return (
     <Wrapper>
-      <h1>{isLoading ? 'Loading...' : 'Users List'}</h1>
-      <button onClick={toggleDiv}>toggle Div</button>
-      {divAppearance ? <div>Jakis tam div</div> : null}
+      <StyledTitle>Students List</StyledTitle>
       <StyledList>
         {users.map((userData) => (
           <UsersListItem deleteUser={deleteUser} key={userData.name} userData={userData} />
