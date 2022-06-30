fce9cb9bcb82a5414ccdfc8b1bd5cc28bc810cb3
Kuba <kubabednarczyk2003@gmail.com>
2022-06-30 17:31:08 +0200

    make FormField Component

--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { users as usersData } from 'data/users';
 import UsersListItem from 'components/molecules/UsersListItem/UsersListItem';
 import { Wrapper, StyledList, StyledTitle } from './UsersList.styles';
+import FormField from 'components/molecules/FormField/FormField';
+import { Button } from 'components/atoms/Button/Button';
 
 const UsersList = () => {
   const [users, setUsers] = useState(usersData);
@@ -12,14 +14,23 @@ const UsersList = () => {
   };
 
   return (
-    <Wrapper>
-      <StyledTitle>Students List</StyledTitle>
-      <StyledList>
-        {users.map((userData) => (
-          <UsersListItem deleteUser={deleteUser} key={userData.name} userData={userData} />
-        ))}
-      </StyledList>
-    </Wrapper>
+    <>
+      <Wrapper>
+        <StyledTitle>Add new student</StyledTitle>
+        <FormField label={'Name'} id={'name'} name={'name'} />
+        <FormField label={'Attendance'} id={'attendance'} name={'attendance'} />
+        <FormField label={'Average'} id={'average'} name={'average'} />
+        <Button>Add</Button>
+      </Wrapper>
+      <Wrapper>
+        <StyledTitle>Students List</StyledTitle>
+        <StyledList>
+          {users.map((userData) => (
+            <UsersListItem deleteUser={deleteUser} key={userData.name} userData={userData} />
+          ))}
+        </StyledList>
+      </Wrapper>
+    </>
   );
 };
 
--- src/components/organisms/UsersList/UsersList.styles.js
+++ src/components/organisms/UsersList/UsersList.styles.js
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
+  margin: 25px;
   background-color: ${({ theme }) => theme.colors.white};
   width: 100%;
   max-width: 500px;
-  margin: 20px 0;
   padding: 40px 50px;
   border-radius: 25px;
   box-shadow: 0 5px 15px -10px rgba(0, 0, 0, 0.3);
