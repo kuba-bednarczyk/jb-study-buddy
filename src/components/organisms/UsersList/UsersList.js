a608b25a298501c1071d9be6b26c7862645ff7f9
Kuba <kubabednarczyk2003@gmail.com>
2022-07-01 16:33:12 +0200

    done forms and routing

--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -1,27 +1,10 @@
-import React, { useState } from 'react';
-import { users as usersData } from 'data/users';
+import React from 'react';
 import UsersListItem from 'components/molecules/UsersListItem/UsersListItem';
 import { Wrapper, StyledList, StyledTitle } from './UsersList.styles';
-import FormField from 'components/molecules/FormField/FormField';
-import { Button } from 'components/atoms/Button/Button';
 
-const UsersList = () => {
-  const [users, setUsers] = useState(usersData);
-
-  const deleteUser = (name) => {
-    const filteredUsers = users.filter((user) => user.name !== name);
-    setUsers(filteredUsers);
-  };
-
+const UsersList = ({ users, deleteUser }) => {
   return (
     <>
-      <Wrapper>
-        <StyledTitle>Add new student</StyledTitle>
-        <FormField label={'Name'} id={'name'} name={'name'} />
-        <FormField label={'Attendance'} id={'attendance'} name={'attendance'} />
-        <FormField label={'Average'} id={'average'} name={'average'} />
-        <Button>Add</Button>
-      </Wrapper>
       <Wrapper>
         <StyledTitle>Students List</StyledTitle>
         <StyledList>
--- src/views/Root.js
+++ src/views/Root.js
@@ -1,25 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import UsersList from 'components/organisms/UsersList/UsersList';
-import styled, { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from 'assets/styles/globalStyle';
 import { theme } from 'assets/styles/theme';
+import { Wrapper } from './Root.styles';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Form from 'components/organisms/Form/Form';
+import { users as usersData } from 'data/users';
 
-const Wrapper = styled.div`
-  background-color: ${({ theme }) => theme.colors.lightGrey};
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100vh;
-`;
+const initialFormState = {
+  name: '',
+  attendance: '',
+  average: '',
+};
 
-const Root = () => (
-  <ThemeProvider theme={theme}>
-    <GlobalStyle />
-    <Wrapper>
-      <UsersList />
-    </Wrapper>
-  </ThemeProvider>
-);
+const Root = () => {
+  const [users, setUsers] = useState(usersData);
+  const [formValues, setFormValues] = useState(initialFormState);
 
+  const deleteUser = (name) => {
+    const filteredUsers = users.filter((user) => user.name !== name);
+    setUsers(filteredUsers);
+  };
+
+  const handleInputChange = (e) => {
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleAddUser = (e) => {
+    e.preventDefault();
+    const newUser = {
+      name: formValues.name,
+      attendance: formValues.attendance,
+      average: formValues.average,
+    };
+
+    setUsers([newUser, ...users]);
+    setFormValues(initialFormState);
+  };
+
+  return (
+    <Router>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Wrapper>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/add-user">Add User</Link>
+          </nav>
+          <Routes>
+            <Route path="/add-user" element={<Form formValues={formValues} handleAddUser={handleAddUser} handleInputChange={handleInputChange} />} />
+            <Route path="/" exact element={<UsersList deleteUser={deleteUser} users={users} />} />
+          </Routes>
+        </Wrapper>
+      </ThemeProvider>
+    </Router>
+  );
+};
 export default Root;
