b0daf601c5b1c639c54b4bbe42da826f38419148
Kuba <kubabednarczyk2003@gmail.com>
2022-07-01 20:09:37 +0200

    fix structure in Root component

--- src/views/Root.js
+++ src/views/Root.js
@@ -48,10 +48,7 @@ const Root = () => {
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Wrapper>
-          <Navigation>
-            <Link to="/">Home</Link>
-            <Link to="/add-user">Add User</Link>
-          </Navigation>
+          <Navigation />
           <Routes>
             <Route path="/add-user" element={<Form formValues={formValues} handleAddUser={handleAddUser} handleInputChange={handleInputChange} />} />
             <Route path="/" exact element={<UsersList deleteUser={deleteUser} users={users} />} />
