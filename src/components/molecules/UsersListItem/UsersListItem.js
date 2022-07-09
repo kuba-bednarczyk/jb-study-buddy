1769f35862ef343b6f492b8a14a517c8b0355f8c
Kuba <kubabednarczyk2003@gmail.com>
2022-07-09 13:21:13 +0200

    make context for components in app

--- src/components/molecules/UsersListItem/UsersListItem.js
+++ src/components/molecules/UsersListItem/UsersListItem.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { StyledAverage, StyledInfo, Wrapper } from './UsersListItem.styles';
 import DeleteButton from 'components/atoms/DeleteButton/DeleteButton';
 import { UserShape } from 'types';
+import { UsersContext } from 'providers/UsersProvider';
 
-const UsersListItem = ({ deleteUser, userData: { average, name, attendance = '0%' } }) => (
-  <Wrapper>
-    <StyledAverage value={average}>{average}</StyledAverage>
-    <StyledInfo>
-      <p>{name}</p>
-      <p>attendance: {attendance}</p>
-    </StyledInfo>
-    <DeleteButton onClick={() => deleteUser(name)}></DeleteButton>
-  </Wrapper>
-);
+const UsersListItem = ({ userData: { average, name, attendance = '0%' } }) => {
+  const { deleteUser } = useContext(UsersContext);
+
+  return (
+    <Wrapper>
+      <StyledAverage value={average}>{average}</StyledAverage>
+      <StyledInfo>
+        <p>{name}</p>
+        <p>attendance: {attendance}</p>
+      </StyledInfo>
+      <DeleteButton onClick={() => deleteUser(name)} />
+    </Wrapper>
+  );
+};
 
 UsersListItem.propTypes = {
   userData: PropTypes.shape(UserShape),
-  deleteUser: PropTypes.func,
 };
 
 export default UsersListItem;
